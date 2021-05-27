25c7499df6c3c34b644c9e7b192280b0fad96ca9
annasemenova <anna.semenova.91@yandex.ru>
2021-05-27 19:59:39 +0300

    lesson 2

---
+++ src/main/java/lesson2/LinkedList/LinkedList.java
@@ -0,0 +1,29 @@
+package lesson2.LinkedList;
+
+public interface LinkedList<E> extends Iterable<E> {
+    void insertFirst(E value);
+
+    E removeFirst();
+
+    boolean remove(E value);
+
+    boolean contains(E value);
+
+    int size();
+
+    boolean isEmpty();
+
+    void display();
+
+    E getFirst();
+
+    class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
---
+++ src/main/java/lesson2/LinkedList/TwoSideLinkedList.java
@@ -0,0 +1,5 @@
+package lesson2.LinkedList;
+
+public interface TwoSideLinkedList<E> extends LinkedList<E> {
+    void insertLast(E value);
+}
